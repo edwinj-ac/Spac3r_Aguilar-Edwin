255e05548520c1cf7b82434d9d6225a54f459212
edwin <edwin@yeltic.com>
2022-08-18 09:28:51 -0500

    Implementacion de API

--- js/form.js
+++ js/form.js
@@ -2,6 +2,13 @@ let miForm  = document.getElementById("form-contact");
 miForm.addEventListener("submit",  evaluateForm);
 
 function evaluateForm(e){
+    var params = {
+        from_name: document.getElementById("name_user").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value,
+        company: document.getElementById("company").value,
+        tel_number: document.getElementById("tel_number").value,
+    };
     e.preventDefault();
     //alert("Datos Enviados");    
     Swal.fire({
@@ -10,4 +17,11 @@ function evaluateForm(e){
         icon: "success",
         confirmButtonText: "Continuar",
     });
+    
+    emailjs.init('MzSoxfF5IP6LpyQOm');
+    emailjs.send("service_ctd0m1e", "template_3krgt6i", params).then(()=>{
+        console.log("envio exitoso");
+    });
+    
+
 }
